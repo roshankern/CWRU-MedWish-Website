ba2c75fa3063b3cb3d4d95a834ed63a721ce568f
Roshan Kern <roshan.kern@ucdenver.edu>
2023-06-13 11:17:35 -0400

    add nav bar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+// import packages
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+// import components
+import NavBar from "./components/NavBar";
+
+// import pages
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -10,32 +16,14 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/events">Events</Link>
-            </li>
-            <li>
-              <Link to="/team">Team</Link>
-            </li>
-          </ul>
-        </nav>
+        <NavBar />
 
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/events" element={<EventsPage />} />
-          <Route path="/team" element={<TeamsPage />} />
+          <Route path="/teams" element={<TeamsPage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+// NavBar.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/events">Events</Link>
+                </li>
+                <li>
+                    <Link to="/teams">Teams</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
