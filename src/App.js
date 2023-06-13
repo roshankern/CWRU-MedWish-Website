08e6939fca7eb68291ee24d7ad4ff47962d9cbfa
Roshan Kern <roshan.kern@ucdenver.edu>
2023-06-13 11:32:00 -0400

    update nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import packages
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // import components
 import NavBar from "./components/NavBar";
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,29 @@
 // NavBar.js
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 function NavBar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/events">Events</Link>
-                </li>
-                <li>
-                    <Link to="/teams">Teams</Link>
-                </li>
-            </ul>
-        </nav>
+        <AppBar position="static">
+            <Toolbar>
+                <Box display="flex" justifyContent="left" flexGrow={1}>
+                    <Button color="inherit" component={RouterLink} to="/">Home</Button>
+                </Box>
+                <Box display="flex" justifyContent="center" flexGrow={1}>
+                    <Button color="inherit" component={RouterLink} to="/about">About</Button>
+                    <Button color="inherit" component={RouterLink} to="/projects">Projects</Button>
+                    <Button color="inherit" component={RouterLink} to="/events">Events</Button>
+                    <Button color="inherit" component={RouterLink} to="/teams">Teams</Button>
+                </Box>
+                <Box display="flex" justifyContent="center" flexGrow={1}>
+                    <Button color="inherit" component="a" href="https://community.case.edu/MedWish/club_signup" target="_blank" rel="noopener noreferrer">Join Us</Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
