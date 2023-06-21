c678e65b4687333f5d8aa89d11ae02f2a8e9482b
Roshan Kern <roshan.kern@ucdenver.edu>
2023-06-20 22:08:36 -0400

    theme

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,26 @@
-// theme.js
+// themes.js
 import { createTheme } from '@mui/material/styles';
+import 'typeface-work-sans';
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#6599CC', // A custom orange color
+            main: '#6599CC', // MedWish light blue
         },
         secondary: {
-            main: '#00355E', // A custom cyan color
+            main: '#00355E', // MedWish dark blue
+        },
+    },
+    typography: {
+        fontFamily: 'Work Sans, sans-serif', // Use Work Sans as the default font
+        h1: {
+            fontSize: '2.5rem', // size for h1 elements
+        },
+        h2: {
+            fontSize: '2rem', // size for h2 elements
+        },
+        body1: {
+            fontSize: '1rem', // size for normal text
         },
     },
 });
