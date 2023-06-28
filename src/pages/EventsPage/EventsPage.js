171018322517a8693293e4d571571adf57d0bad0
Khanh Khuat <lankhanh1504@gmail.com>
2023-06-27 21:29:09 -0400

    Update Projects and Events (#4)
    Add page headers for projects and events

--- src/pages/EventsPage/EventsPage.js
+++ src/pages/EventsPage/EventsPage.js
@@ -1,8 +1,21 @@
-// AboutPage.js
+// EventsPage.js
 import React from "react";
+import "./EventsPage.css"; // Import the CSS file for styling
+
 
 function AboutPage() {
-    return <h1>Welcome to the Events Page!</h1>;
+    return (
+        <div className="about-page">
+            <img src="../assets/EventImage.png" alt="Cover Picture" />
+            <hr className="line" />
+            <div className="content">
+                <p className="text">
+CWRU MedWish is active year round! Check out some of our upcoming events below to see how you can help your community and the world. More information about upcoming events can be found at the CWRU MedWish CampusGroup page:
+https://community.case.edu/medwish/home                </p>
+                <hr className="line" />
+            </div>
+        </div>
+    );
 }
 
 export default AboutPage;
