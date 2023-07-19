4cfd2ae5a636a42344eb72885594614fb90172d1
Khanh Khuat <lankhanh1504@gmail.com>
2023-07-18 22:01:56 -0400

    Update Events and Projects Page (#14)
    * Initial commit
    
    * update projects and events
    
    * update projects and events page

--- src/pages/EventsPage/EventsPage.js
+++ src/pages/EventsPage/EventsPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardMedia, Typography, Divider, Box, IconButton, Grid, CardContent} from '@mui/material';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { Card, CardMedia, Typography, Divider, Box, Grid, CardContent} from '@mui/material';
 import EventImage from './images/EventImage.png';
 import EventCard from './EventCard';
 
@@ -8,7 +7,7 @@ import EventCard from './EventCard';
 function EventsPage() {
 
   /**
-   * Fetch events from Google Sheets
+   * Fetch events from Google Sheets.
    */
   const [events, setEvents] = useState([]);
 
@@ -29,7 +28,7 @@ function EventsPage() {
   }, []);
 
   /**
-   * Group events by month
+   * Groups events by month.
    */
   const groupedEvents = events.reduce((acc, event) => {
     const eventMonth = new Date(event.date).toLocaleString('default', { month: 'long' });
@@ -39,7 +38,7 @@ function EventsPage() {
     acc[eventMonth].push(event);
     return acc;
   }, {});
-
+  
   return (
     <div>
     {/* Events Image */}
@@ -61,25 +60,6 @@ function EventsPage() {
     </Box>
       <Divider style={{ borderTopWidth: '1px', borderTopColor: 'gray', height: '0px', margin: '30px 0', color: 'white' }} />
 
-      {/* Add to Calendar Button */}
-      <Box display="flex" justifyContent="center">
-         <IconButton
-          sx={{
-            bgcolor: '#6599CC',
-            color: 'white',
-            borderRadius: '20px', // Adjust the value to control the roundness of the corners
-            '&:hover': {
-              bgcolor: '#478db8', // Adjust the hover color if needed
-            },
-          }}
-          variant="contained"
-          size="large"
-        >
-          <CalendarMonthIcon />
-          <Typography variant="body1">ADD TO GOOGLE CALENDAR</Typography>
-        </IconButton>
-      </Box>
-
       {/* Events */}
       {Object.entries(groupedEvents).map(([month, monthEvents]) => (
         <Card key={month} sx={{ mb: 4 }}>
@@ -92,7 +72,7 @@ function EventsPage() {
                     name={event.name}
                     date={event.date}
                     description={event.description}
-                    image={event['image link']}
+                    image={event['image_link']}
                   />
                 </Grid>
               ))}
