08013f33e261ffff1e6dd83ecea0c855b5afc6ad
roshankern <roshan.kern@ucdenver.edu>
2023-07-18 22:48:05 -0400

    update header and footer

--- src/components/CustomAppBar/CustomAppBar.js
+++ src/components/CustomAppBar/CustomAppBar.js
@@ -13,11 +13,11 @@ function CustomAppBar() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Box display="flex" justifyContent="left" flexGrow={1}>
+                <Box display="flex" justifyContent="left">
                     <Button color="secondary" component={RouterLink} to="/" startIcon={<img src={MedWishLogo} alt="MedWish Logo" style={{ height: "50px", objectFit: 'contain' }} />} />
                 </Box>
 
-                <Box display="flex" justifyContent="center" flexGrow={1}>
+                <Box display="flex" justifyContent="center" flexGrow={2}>
                     <Button color="secondary" component={RouterLink} to="/about" sx={{ textTransform: 'none' }}>About</Button>
                     <Divider />
                     <Button color="secondary" component={RouterLink} to="/projects" sx={{ textTransform: 'none' }}>Projects</Button>
@@ -26,9 +26,32 @@ function CustomAppBar() {
                     <Divider />
                     <Button color="secondary" component={RouterLink} to="/team" sx={{ textTransform: 'none' }}>Team</Button>
                 </Box>
-                <Box display="flex" justifyContent="right" flexGrow={1}>
-                    <Button color="secondary" component="a" href="https://community.case.edu/MedWish/club_signup" target="_blank" rel="noopener noreferrer">Join</Button>
-                </Box>
+
+                <a 
+    href="https://community.case.edu/MedWish/club_signup"
+    target="_blank"
+    rel="noopener noreferrer"
+    style={{ textDecoration: 'none' }}  // Removes underline from the link
+>
+    <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        bgcolor="#00355E"
+        borderRadius="16px"
+        px={2}
+        py={1}
+        width="100px"
+        cursor="pointer"  // Changes the cursor to a hand when hovering over the box
+    >
+        <Button
+            color="primary"
+            sx={{ textTransform: 'none' }}  // No uppercasing of the text
+        >
+            Join
+        </Button>
+    </Box>
+</a>
             </Toolbar>
         </AppBar>
     );
