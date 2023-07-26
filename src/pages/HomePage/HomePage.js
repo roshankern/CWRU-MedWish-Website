5094520f31c2770c693e8b4796fbab26c30cc6ab
jjessicas <jks179@case.edu>
2023-07-25 19:29:26 -0700

    HomePage
    I added the header image and text to the home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,50 @@
-// HomePage.js
 import React from "react";
+import HomePageImage from './images/homepage-image_photo_noise2_scale_cropped_photo_noise2_scale.png';
 
-function HomePage() {
-    return <h1>Welcome to the new Home Page!</h1>;
+const imageContainerStyle = {
+  position: "relative",
+  width: "100%",
+};
 
+const imageStyle = {
+  width: "100%",
+  height: "auto",
+  objectFit: "cover",
+};
+
+const overlayStyle = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust the alpha value (0.5) to control darkness
+};
+
+const textStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  fontFamily: "Work Sans, sans-serif",
+  color: "white",
+  fontSize: "52px",
+  fontWeight: "bold",
+  zIndex: 1,
+};
+
+function TopImage() {
+  return (
+    <div style={imageContainerStyle}>
+      <img
+        src={HomePageImage} 
+        alt="Home Page Image"
+        style={imageStyle}
+      />
+      <div style={overlayStyle}></div>
+      <h1 style={textStyle}>CWRU Medwish</h1>
+    </div>
+  );
 }
 
-export default HomePage;
+export default TopImage;
