1a92f96d038d43f1af60d84a885484c70f63f58b
roshankern <roshan.kern@ucdenver.edu>
2023-07-25 20:40:58 -0400

    change home page message

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 function HomePage() {
-    return <h1>Welcome to the Home Page!</h1>;
+    return <h1>Welcome back to the best home page!</h1>;
 }
 
 export default HomePage;
