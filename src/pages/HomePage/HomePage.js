698517a72b3aec718b804ce75f30df264731c839
Roshan Kern <roshan.kern@ucdenver.edu>
2023-08-29 09:45:27 -0400

    update content

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography, Grid } from '@mui/material';
 
 import HomePageImage from './images/HomePage.png';
-import HomeImage1 from './images/HomeImage1.JPG';
+import HomeImage1 from './images/HomeImage1.png';
 import HomeImage2 from './images/HomeImage2.png';
 
 import HeaderComponent from "../../components/HeaderComponent.js";
@@ -19,7 +19,7 @@ function HomePage() {
 
       <HeaderComponent title="About Us" orientation="left" />
 
-      <Grid container spacing={3}>
+      <Grid container spacing={3} style={{ paddingLeft: '20px', paddingRight: '20px' }} alignItems="center">
         <Grid item xs={12} md={6}>
           <div className="paragraph-text">
             <p>We are a group of Case Western Reserve University students who work with the not-for-profit organization MedWish International to repair discarded medical equipment and ship this equipment to countries in need.</p>
@@ -27,11 +27,11 @@ function HomePage() {
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <img src={HomeImage1} className="header-image" alt="Header" />
+          <img src={HomeImage1} className="header-image" alt="Header" style={{ borderRadius: '20px' }} />
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <img src={HomeImage2} className="header-image" alt="Header" />
+          <img src={HomeImage2} className="header-image" alt="Header" style={{ borderRadius: '20px' }} />
         </Grid>
 
         <Grid item xs={12} md={6}>
@@ -39,10 +39,7 @@ function HomePage() {
             <p>CWRU MedWish takes volunteer trips downtown to the headquarters of MedWish International. We triage, test, repair and package medical devices. In addition to our regular trips, we host informational workshops for members to learn more about biomedical engineering.</p>
           </div>
         </Grid>
-
-
       </Grid>
-
 
 
       <div className="spacer-line"> </div>
