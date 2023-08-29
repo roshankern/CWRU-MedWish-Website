3e0bc06326039ef1ff51bc08a03f44c6bfc41930
Roshan Kern <roshan.kern@ucdenver.edu>
2023-08-29 09:30:03 -0400

    add content

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React from "react";
-import HomePageImage from './images/HomePage.png';
 import Typography from "@mui/material/Typography";
+
+import HomePageImage from './images/HomePage.png';
+import HomeImage1 from './images/HomeImage1.JPG';
+import HomeImage2 from './images/HomeImage2.png';
 
 import HeaderComponent from "../../components/HeaderComponent.js";
 import "../../styles.css";
@@ -15,6 +18,20 @@ function HomePage() {
       <div className="spacer-line"> </div>
 
       <HeaderComponent title="About Us" orientation="left" />
+
+      <div className="paragraph-text">
+        <p>CWRU MedWish promotes healthcare equity on an international scale assisting MedWish International in processing medical devices. Our priority is to increase access to healthcare globally by repurposing locally donated medical equipment. CWRU MedWish assists in repairing and packaging priority medical devices for use in underserved regions. Our methods of device processing are based on a comprehensive and officially verified understanding of medical device guidelines.</p>
+      </div>
+
+      <img src={HomeImage1} className="header-image" alt="Header" />
+
+      <div className="paragraph-text">
+        <p>CWRU MedWish promotes healthcare equity on an international scale assisting MedWish International in processing medical devices. Our priority is to increase access to healthcare globally by repurposing locally donated medical equipment. CWRU MedWish assists in repairing and packaging priority medical devices for use in underserved regions. Our methods of device processing are based on a comprehensive and officially verified understanding of medical device guidelines.</p>
+      </div>
+
+      <img src={HomeImage2} className="header-image" alt="Header" />
+
+
 
       <div className="spacer-line"> </div>
       <div className="spacer"></div>
