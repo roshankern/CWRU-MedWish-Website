4ac884156a0b3f544ee17da0ce15dfd702e3f537
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-08-31 11:42:30 -0400

    changes to depoly website (#28)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // App.js
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
 import CustomAppBar from "./components/CustomAppBar/CustomAppBar.js";
 import Footer from "./components/Footer/Footer.js";
 import HomePage from "./pages/HomePage/HomePage.js";
@@ -17,12 +18,15 @@ function App() {
       <Router>
         <CustomAppBar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/cwru-medwish-website" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/team" element={<TeamsPage />} />
         </Routes>
+
         <Footer />
       </Router>
     </ThemeProvider>
